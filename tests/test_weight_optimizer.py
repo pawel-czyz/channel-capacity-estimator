784702502c23ab4aefe04c41abed4fcf28876414
Frederic Grabowski <grabowski.frederic@gmail.com>
2018-08-07 23:46:46 +0200

    cleaning up many gaussian test

--- tests/test_weight_optimizer.py
+++ tests/test_weight_optimizer.py
@@ -45,21 +45,20 @@ class TestWeightOptimizer(unittest.TestCase):
         self.assertAlmostEqual(ws["bunny"], 0.50, delta=0.01)
 
 #    def test_gaussians(self):
-#        print("Calculate optimal MI for a couple Gaussians")
+#        """Find optimal MI for a couple of Gaussians"""
 #        def _mu_sigma(i):
 #            return (1.5*(i+1), (i+1)/4. + 1)
 #
-#        subsamples_counts = 10 * np.array([257, 563, 533, 440, 537, 625, 503,
-#            747])
+#        subsamples_counts = 10 * np.array([257, 563, 533, 440,
+#                                           537, 625, 503, 747])
 #
 #        data = []
-#        for i in range(len(subsamples_counts)):
-#            for v in norm(*_mu_sigma(i)).rvs(subsamples_counts[i]):
+#        for i, cnt in enumerate(subsamples_counts):
+#            for v in norm(*_mu_sigma(i)).rvs(cnt):
 #                data.append((i, [v]))
 #
 #        wke = WeightedKraskovEstimator(data)
-#        wke.calculate_neighborhood(k=200)
-#        mi, ws = wke.optimise_weights()
+#        mi, ws = wke.calculate_maximized_mi(k=50)
 #        print(mi, ws)
 
 if __name__ == '__main__':
